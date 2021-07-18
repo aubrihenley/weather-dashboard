b1ba18e758e527c31d154a4436ea6b0c69ca7fe5
Aubri Henley <aubri.henley@gmail.com>
2021-07-18 18:39:42 -0400

    added api call to set up inputs and verify API is working

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,63 @@
-// https://openweathermap.org/api/one-call-api
+// // https://openweathermap.org/api/one-call-api   APIKey 5c5a571775e8eded1b0f694bee36a7b4
+// //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// // search by city name api.openweathermp.org/daata/2.5/weather?q={city name}&appid={API key}
 
-// create buttons for previously searched city
+// var APIKey ='5c5a571775e8eded1b0f694bee36a7b4';
+// var userCityInput =document.getElementById('searchCity');
 
-var searchCityEl =$('#search-form');
-var searchHistoryEl = $('#addBtnSearch')
+// var currentWeather = document.getElementById('current-city-weather');
+// var fetchButton = document.getElementById('searchBtn');
 
-function handleCitySubmit(event) {
-    event.preventDefault();
+// function getApi() {
+//     var userInput =userCityInput.Value.trim();
+//   // fetch request gets gets local forecast based on user input.
+//   var requestUrl = 'https://api.openweathermp.org/daata/2.5/weather?q=' + userInput +'&appid=' + APIKey ;
+// console.log(requestUrl);
+//   fetch(requestUrl)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data)
 
-    var searchedCity = $('input[name="searchCityInput"]').val();
+//   })
+//   .on('click', getApi)
+// }
+// // create buttons for previously searched city
 
-    if (!searchedCity) {
-        console.log('Please enter a city!');
-        return;
+// var searchCityEl =$('#search-form');
+// var searchHistoryEl = $('#addBtnSearch')
+
+// function handleCitySubmit(event) {
+//     event.preventDefault();
+
+//     var searchedCity = $('input[name="searchCityInput"]').val();
+
+//     // if (!searchedCity) {
+//     //     console.log('Please enter a city!');
+//     //     return;
+//     // }
+
+//     searchHistoryEl.append('<button type="button" class="btn btn-primary">' + searchedCity + '</button>');
+
+//     // clears input element
+//     $('input[name="searchCityInput"]').val('');
+//  }
+
+// searchCityEl.on('click', handleCitySubmit)
+
+
+function getApi() {
+    // fetch request gets a list of all the repos for the node.js organization
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=asheboro&appid=5c5a571775e8eded1b0f694bee36a7b4';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+      });
     }
 
-    searchHistoryEl.append('<button type="button" class="btn btn-primary">' + searchedCity + '</button>');
-
-    // clears input element
-    $('input[name="searchCityInput"]').val('');
-}
-
-searchCityEl.on('click', handleCitySubmit)
+    getApi();
