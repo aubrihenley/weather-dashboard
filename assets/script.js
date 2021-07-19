91c5c6b66a8df3720d7a15b11a979f914595da11
Aubri Henley <aubri.henley@gmail.com>
2021-07-19 17:20:04 -0400

    created global selectors, rendered current day to page, fetch base API

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,55 @@
 // //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 // // search by city name api.openweathermp.org/daata/2.5/weather?q={city name}&appid={API key}
 
-// var APIKey ='5c5a571775e8eded1b0f694bee36a7b4';
-// var userCityInput =document.getElementById('searchCity');
+var APIKey ='5c5a571775e8eded1b0f694bee36a7b4';
 
-// var currentWeather = document.getElementById('current-city-weather');
-// var fetchButton = document.getElementById('searchBtn');
+//search bar selectors
+var userCityInputEl =$('#searchCity');
+var searchButtonEl = $('#searchBtn');
+var searchCityEl =$('#search-form');
+var searchHistoryEl = $('#addBtnSearch');
+
+//current weather selectors
+var currentWeather = $('#current-city-weather');
+var cityEl = $('#currentCityName');
+var currentTempEl = $('#currentTemp');
+var currentWindEl = $('#currentWindSpeed');
+var currentHumidityEl = $('#currentHumidity') ;
+var currentUviEl = $('#currentUVI');
+
+//5-day Forecast selectors
+var forecastDateEl = $('#forecast-date');
+var forecastIconEl = $('.forecast-icon');
+var forecastTempEl = $('#forecast-temp');
+var forecastTempEl = $('#forecast-wind');
+var forecastHumidityEl = $('#forecast-humidity');
+
+var today = moment();
+$("#currentDate").text(today.format("MMM Do, YYYY"));
+
+
+function getApi() {
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=asheboro&units=imperial&appid=' + APIKey;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data){
+      console.log(data)
+      for (var i = 0; i < data.length; i++) {
+      
+      console.log(data[i].main.name);
+
+
+      }
+    });
+}
+getApi();
 
 // function getApi() {
 //     var userInput =userCityInput.Value.trim();
-//   // fetch request gets gets local forecast based on user input.
+//   // fetch request gets gets Latitude and longitude based on user input.
 //   var requestUrl = 'https://api.openweathermp.org/daata/2.5/weather?q=' + userInput +'&appid=' + APIKey ;
 // console.log(requestUrl);
 //   fetch(requestUrl)
@@ -47,17 +87,24 @@
 // searchCityEl.on('click', handleCitySubmit)
 
 
-function getApi() {
-    // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=asheboro&appid=5c5a571775e8eded1b0f694bee36a7b4';
+// function getApi() {
+//     // fetch request gets a list of all the repos for the node.js organization
+//     var requestUrlLatLon = 'https://api.openweathermap.org/data/2.5/weather?q=asheboro&units=imperial&appid=' + APIKey;
   
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-      });
-    }
+//     fetch(requestUrlLatLon)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         console.log('longitude & latitude \n----------') 
+//         for (var i = 0; i < data.length; i++) {
+//           console.log(data[i].coord.lat);
+//           console.log(data[i]);
+//       };
+//     });
+// }
+    
 
-    getApi();
+//     getApi();
+
+    // var requestOneCall = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ latitude +'&lon=' + longitude + '&units=imperial&exclude=hourly,minutely,alerts&appid=5c5a571775e8eded1b0f694bee36a7b4'
