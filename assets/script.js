0ec43e3a9d207baaee7ffa37956997e9e9f64876
Aubri Henley <aubri.henley@gmail.com>
2021-08-02 22:26:09 -0400

    updated forecast and buttons

--- assets/script.js
+++ assets/script.js
@@ -19,13 +19,6 @@ var currentHumidityEl = $('#currentHumidity');
 var currentUviEl = $('#currentUVI');
 var currentIcon = $('#currentWeatherIcon');
 
-//5-day Forecast selectors
-var forecastDateEl = $('#forecast-date');
-var forecastIconEl = $('.forecast-icon');
-var forecastTempEl = $('#forecast-temp');
-var forecastTempEl = $('#forecast-wind');
-var forecastHumidityEl = $('#forecast-humidity');
-
 var today = moment();
 $("#currentDate").text(today.format("MMM Do, YYYY"));
 
@@ -39,39 +32,64 @@ function getApi(spaghetti) {
       return response.json();
     })
     .then(function (data) {
-        console.log(data);
-        console.log("data type", typeof data);
+      console.log(data);
+      console.log("data type", typeof data);
 
-        cityEl.text(data.name);
+      cityEl.text(data.name);
 
-        var cityLat = (data.coord.lat);
-        var cityLong = (data.coord.lon);
-        var oneCallURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLong + '&units=imperial&exclude=hourly,minutely,alerts&appid=' + APIKey;
+      var cityLat = (data.coord.lat);
+      var cityLong = (data.coord.lon);
+      var oneCallURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLong + '&units=imperial&exclude=hourly,minutely,alerts&appid=' + APIKey;
 
-        fetch(oneCallURL)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (data) {
-              console.log(data);
+      fetch(oneCallURL)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          currentTempEl.text(data.current.temp + '\xB0' + 'F');
+          currentWindEl.text(data.current.wind_speed);
+          currentHumidityEl.text(data.current.humidity);
+          currentUviEl.text(data.current.uvi);
 
-              currentTempEl.text(data.current.temp + '\xB0' + 'F');
-              currentWindEl.text(data.current.wind_speed);
-              currentHumidityEl.text(data.current.humidity);
-              currentUviEl.text(data.current.uvi);
-              var imageCode = data.current.weather[0].icon;
-              console.log(imageCode);
-              var imageURL= 'http://openweathermap.org/img/wn/'+ imageCode +'@2x.png'
+          var imageCode = data.current.weather[0].icon;
+          var imageURL = 'http://openweathermap.org/img/wn/' + imageCode + '@2x.png';
 
-            $('#currentWeatherIcon').empty()
+          $('#currentWeatherIcon').empty()
+          var image = new Image();
+          image.src = imageURL;
+          document.getElementById('currentWeatherIcon').appendChild(image);
+
+
+          //for daily forecast
+          for (var i = 1; i < 6; i++) {
+            var dailyDate = data.daily[i].dt;
+            console.log(dailyDate);
+            var milliseconds = dailyDate * 1000;
+            var dateObject = new Date(milliseconds);
+            var humanDate = dateObject.toLocaleString("en-US", {
+              Weekday: "long",
+              day: "numeric",
+              month: "numeric",
+              year: "numeric"
+            });
+            document.getElementById('forecastDate' + i).innerHTML = humanDate;
+            document.getElementById('forecastTemp' + i).innerHTML = data.daily[i].temp.day + '\xB0' + 'F';
+            document.getElementById('forecastWind' + i).innerHTML = data.daily[i].wind_speed;
+            document.getElementById('forecastHumidity' + i).innerHTML = data.daily[i].humidity;
+
+            var dailyImage = data.daily[i].weather[0].icon;
+            var dailyImageURL = 'http://openweathermap.org/img/wn/' + dailyImage + '@2x.png';
+            console.log(dailyImage);
+            //$('#forecast-icon'+i).empty()
             var image = new Image();
-            image.src = imageURL;
-            document.getElementById('currentWeatherIcon').appendChild(image);
+            image.src = dailyImageURL;
+            document.getElementById('dailyIcon' +i).appendChild(image);
             $("img").addClass("float-right");
-
-          })
+          };
+        });
     });
-};
+}
 // getApi();
 
 searchButtonEl.on('click', function () {
@@ -83,28 +101,28 @@ searchButtonEl.on('click', function () {
 })
 
 
-//creat a variable as the city key from local storage
-//create button 
-//append that button 
+// create a variable as the city key from local storage
+// create button 
+// append that button 
 
 
-// // create buttons for previously searched city
+// create buttons for previously searched city
 
-// var searchCityEl =$('#search-form');
-// var searchHistoryEl = $('#addBtnSearch')
+var searchCityEl =$('#search-form');
+var searchHistoryEl = $('#addBtnSearch')
 
-// function handleCitySubmit(event) {
-//     var searchedCity = $('input[name="searchCityInput"]').val();
+function handleCitySubmit(event) {
+    var searchedCity = $('input[name="searchCityInput"]').val();
 
-//     // if (!searchedCity) {
-//     //     console.log('Please enter a city!');
-//     //     return;
-//     // }
+    // if (!searchedCity) {
+    //     console.log('Please enter a city!');
+    //     return;
+    // }
 
-//     searchHistoryEl.append('<button type="button" class="btn btn-primary">' + searchedCity + '</button>');
+    searchHistoryEl.append('<button type="button" class="btn btn-primary">' + searchedCity + '</button>');
 
-//     // clears input element
-//     $('input[name="searchCityInput"]').val('');
-//  }
+    // clears input element
+    $('input[name="searchCityInput"]').val('');
+ }
 
-// searchCityEl.on('click', handleCitySubmit)
+searchCityEl.on('click', handleCitySubmit)
